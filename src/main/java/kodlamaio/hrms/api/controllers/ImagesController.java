37949bdd6c61ea7c00d1cf39f858453d321190e5
Çetin Ürün <cetinurun.12@outlook.com>
2021-06-04 21:35:39 +0300

    Controllers added for new entities.

---
+++ src/main/java/kodlamaio/hrms/api/controllers/ImagesController.java
@@ -0,0 +1,36 @@
+package kodlamaio.hrms.api.controllers;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import kodlamaio.hrms.business.abstracts.ImageService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.entities.concretes.Image;
+
+@RestController
+@RequestMapping("/api/images")
+public class ImagesController {
+
+	private ImageService imageService;
+
+	@Autowired
+	public ImagesController(ImageService imageService) {
+		super();
+		this.imageService = imageService;
+	}
+	@GetMapping("/getall")
+	public DataResult<List<Image>> getAll() {
+		
+		return this.imageService.getAll();
+	}
+
+	@PostMapping("/add")
+	public Result add(@RequestBody Image image) {
+		
+		return this.imageService.add(image);
+	}
+}
